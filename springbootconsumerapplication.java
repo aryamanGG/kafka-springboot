0ed85638b24377c9b24cab5b275f1c6ffd39df49
Aryaman Gautam <120401190+aryamanGG@users.noreply.github.com>
2025-08-13 05:15:13 +0530

    Add files via upload

---
+++ springbootconsumerapplication.java
@@ -0,0 +1,11 @@
+package net.aryaman.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootconsumerapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(springbootconsumerapplication.class, args);
+    }
+}
